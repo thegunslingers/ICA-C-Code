2ce2fa1c3c567b660f27c402111a0df7e0d4cf5b
Geddings <cbarrin@g.clemson.edu>
2015-04-06 12:28:33 -0400

    Changed PCABigfn.c

--- pcabigFn.c
+++ pcabigFn.c
@@ -75,6 +75,7 @@ void pcabigFn(data_t **U, data_t **R, data_t **E, int rows, int cols, data_t **B
 	data_t *temp_vec;
 	data_t *index;
 	data_t *Vsort;
+	data_t *U_squared;
 	
 	data_t *righteignm;
 	data_t *length_matrix;
@@ -92,18 +93,18 @@ void pcabigFn(data_t **U, data_t **R, data_t **E, int rows, int cols, data_t **B
 	// Rows by 1 array of ones
 	allocate_matrix(&length_ones, rows, 1);
 	// temp vars are used to move matrix from on function to another
-	allocate_matrix(&temp, rows, cols);
 	allocate_matrix(&temp_vec, 1, cols);
 	
     allocate_matrix(&index, 1, cols);			//  [N,P] = size(B);    -   sizes found in matlab code                 
-	allocate_matrix(&Vsort, cols, cols);
+	allocate_matrix(&Vsort, num_images, num_images);
 	// image data transposed
 	allocate_matrix(&B_zmtrans, num_images, num_pixels);
 	allocate_matrix(&B_mult, num_images, num_images);
 	allocate_matrix(&B_div, num_images, num_images);
 	allocate_vector(&B_vector, num_images*num_images);
 	
-	allocate_matrix(&U, rows, cols);
+	allocate_matrix(&U, num_pixels, num_images);
+	allocate_matrix(&U_squared, num_pixels, num_images);
 	allocate_matrix(&righteignm, cols, cols);
 	allocate_matrix(&length_matrix, rows, cols);
 	allocate_matrix(&R, cols, cols);
@@ -138,20 +139,20 @@ void pcabigFn(data_t **U, data_t **R, data_t **E, int rows, int cols, data_t **B
                                                 //  eigvalvec = max(D); -   handled by lapack function
 	                                        	//  [seigvals, index] = sort(eigvalvec); % sort goes low to high
 	eigSort(righteignm, (data_t*)wr, num_images, num_pixels);
-	fliplr(index, index, rows, cols);
+	//fliplr(index, index, rows, cols);
 	
-	fliplr(Vsort, righteignm, rows, cols);
+	fliplr(Vsort, righteignm, num_images, num_images);
 												//Vsort = V(:,fliplr(index));
     /*  !will use mergesort to sort eigenvalues... when two eigenvalues are switched    !
         !the corresponding eigenvectors also will need to be switched                   !   */
 
                                                 //  %********Reconstruct
-    multiply_matrices(U, matrix, Vsort, rows, cols, cols);        //  U = B*Vsort;  % Cols of U are Bvi. (N-dim Eigvecs)
+    multiply_matrices(U, B_zeromean, Vsort, num_pixels, num_images, num_images);        //  U = B*Vsort;  % Cols of U are Bvi. (N-dim Eigvecs)
                                                 //
                                                 //  %********Give eigvecs unit length.  Improves classification.
 												//  length = sqrt (sum (U.^2)); 
 												//temp = sum_along_rows(temp); <<worng?
-	raise_matrix_to_power(temp, U, rows, cols, 2);
+	raise_matrix_to_power(U_squared, U, num_pixels, num_images, 2);
 	sum_columns(temp, temp_vec, rows, cols);
 	matrix_sqrt(temp_vec, temp_vec, rows, cols);
     
